1e7bbc7853636a928134de8697839f8613d9cae3
Daniel Hernandez <daniel.hhuerta@gmail.com>
2022-08-30 10:48:24 +0200

    Añadida entidad estudiante

---
+++ src/main/java/com/danielhh/apirestEstudiante/model/Estudiante.java
@@ -0,0 +1,69 @@
+package com.danielhh.apirestEstudiante.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="estudiante")
+public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombres;
+    private String apellidos;
+    private String email;
+    private Double nota;
+
+    public Estudiante() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public void setNota(Double nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" +
+                "id=" + id +
+                ", nombres='" + nombres + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", email='" + email + '\'' +
+                ", nota=" + nota +
+                '}';
+    }
+}
