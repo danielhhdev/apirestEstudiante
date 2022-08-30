99b18ff4183f5fe6b5d6a23abe8369af2f8e09bf
Daniel Hernandez <daniel.hhuerta@gmail.com>
2022-08-30 09:14:12 +0200

    proyecto en blanco

---
+++ src/main/java/com/danielhh/apirestEstudiante/ApirestEstudianteApplication.java
@@ -0,0 +1,13 @@
+package com.danielhh.apirestEstudiante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestEstudianteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestEstudianteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielhh/apirestEstudiante/ApirestEstudianteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielhh.apirestEstudiante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestEstudianteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
